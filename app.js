3c960cab460eec198ca1d3c2e06adf2c4e04c7ff
frizmob@gmail.com <friz48625>
2022-07-30 12:48:12 +0300

    v1.1 связали с html

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const App = {
     // data() - метод передачи данных в html
     data() {
         return {
-            counter: 0
+            counter: 0,
+            title: 'Счетчик'
         }
     }
-
 }
 
-const app = Vue.createApp(App)
-app.mount('#app')
+// const app = Vue.createApp(App)
+// app.mount('#app')
+// (или)
+Vue.createApp(App).mount('#app')
