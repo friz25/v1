ba0ad6f5ccd757fa814982397b5bbd6ed2a22fea
frizmob@gmail.com <friz48625>
2022-07-31 13:31:23 +0300

    v3.3 v-for (особенности)

--- 3/3.js
+++ 3/3.js
@@ -1,6 +1,12 @@
 Vue.createApp({
     data: () => ({
         myHtml: '<h1>Vue 3 App</h1>',
-        title: 'Я есть Грут'
+        title: 'Я есть Грут',
+        person: {
+            firstName: 'Victor',
+            lastName: 'Dzeba',
+            age: 30
+        },
+
     })
 }).mount('#app')
