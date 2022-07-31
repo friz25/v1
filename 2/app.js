3d44b426db1feb320240d2b209d6b4fb684d350a
frizmob@gmail.com <friz48625>
2022-07-31 11:16:01 +0300

    v2.1 двухстороннее связывание (вводим текст>выводиться что ввели)

--- 2/app.js
+++ 2/app.js
@@ -4,7 +4,14 @@ const App = {
     data() {
         return {
             placeholderString: 'Введите название заметки...',
-            title: 'Список заметок'
+            title: 'Список заметок',
+            inputValue: ''
+        }
+    },
+    methods: {
+        inputChangeHandler(event) {
+            // console.log('inputChangeHandler', event.target.value)
+            this.inputValue = event.target.value
         }
     }
 }
