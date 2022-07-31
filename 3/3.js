c7cb25b84b4c44e12a6ed8d058bdfb82ddd4b4e9
frizmob@gmail.com <friz48625>
2022-07-31 13:18:15 +0300

    v3.1 - v-text - v-once - v-pre - v-html

--- 3/3.js
+++ 3/3.js
@@ -1,10 +1,6 @@
 Vue.createApp({
-    // data() {
-    //     return {
-    //
-    //     }
-    // }
     data: () => ({
+        myHtml: '<h1>Vue 3 App</h1>',
         title: 'Я есть Грут'
     })
 }).mount('#app')
